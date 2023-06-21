51d9309bf762ae2d66be52231464fa600d25775b
Conrado F. Moreno-Fernández <cfmorenofernandez@gmail.com>
2023-06-21 13:21:14 -0500

    Update test.js

--- test.js
+++ test.js
@@ -7,17 +7,7 @@ xhr.onload = function() {
     var response = JSON.parse(xhr.responseText);
     console.log(response); // Display the response in the console
     
-    // You can now use the response data as needed
-    // For example, iterate over the response and display the data on the webpage
-    for (var i = 0; i < response.length; i++) {
-      var row = response[i];
-      console.log(row); // Display each row in the console
-      // Access the values of each row and do something with them
-      var name = row.Name;
-      var class = row.Class;
-      // ...
-      // Perform further operations with the data
-    }
+    
   } else {
     console.error('Request failed. Status:', xhr.status);
   }
