ad046b37690c6da21ea6f5af4cdb24494c522037
Conrado F. Moreno-Fernández <cfmorenofernandez@gmail.com>
2023-06-21 11:39:44 -0500

    Update test.php

--- test.php
+++ test.php
@@ -1,12 +1,4 @@
 <?php
-function debug_to_console($data) {
-    $output = $data;
-    if (is_array($output))
-        $output = implode(',', $output);
-
-    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
-}
-
 try {
     $conn = new PDO("sqlsrv:server = tcp:mirarlol.database.windows.net,1433; Database = champion_db", "joacoenriqueconrado1", "Siempregptarqui1");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -14,9 +6,8 @@ try {
     $stmt = $conn->query("EXEC GetStatistics");
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
     
-    debug_to_console($result); // Log the result to the web console
-    
-    echo json_encode($result);
+    header('Content-Type: application/json'); // Set the response content type to JSON
+    echo json_encode($result); // Encode the result as JSON and echo it
 } catch (PDOException $e) {
     die("Error connecting to SQL Server: " . $e->getMessage());
 }
