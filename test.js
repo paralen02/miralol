af67911363eb6dab0ead90c566a09892d8eed8f7
Conrado F. Moreno-Fernández <cfmorenofernandez@gmail.com>
2023-06-21 12:50:56 -0500

    Update test.js

--- test.js
+++ test.js
@@ -10,35 +10,8 @@ document.addEventListener("DOMContentLoaded", function() {
             if (result.error) {
                 console.log("Error: " + result.error);
             } else {
-                let table = document.createElement("table");
-                let thead = document.createElement("thead");
-                let tbody = document.createElement("tbody");
-
-                // Create table headers
-                let headers = Object.keys(result[0]);
-                let headerRow = document.createElement("tr");
-                headers.forEach(header => {
-                    let th = document.createElement("th");
-                    th.textContent = header;
-                    headerRow.appendChild(th);
-                });
-                thead.appendChild(headerRow);
-                table.appendChild(thead);
-
-                // Create table rows and cells
-                result.forEach(row => {
-                    let rowElement = document.createElement("tr");
-                    Object.values(row).forEach(value => {
-                        let cell = document.createElement("td");
-                        cell.textContent = value;
-                        rowElement.appendChild(cell);
-                    });
-                    tbody.appendChild(rowElement);
-                });
-                table.appendChild(tbody);
-
-                // Append the table to the document
-                document.getElementById("result").appendChild(table);
+                let output = JSON.stringify(result, null, 2); // Convert JSON to formatted string
+                document.getElementById("result").textContent = output; // Use textContent to display the JSON string
             }
         })
         .catch(error => console.log(error));
