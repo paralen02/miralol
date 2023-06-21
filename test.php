3abaa1c14d85e1e17c1d32fd1886bdc5ca3db31a
Conrado F. Moreno-Fernández <cfmorenofernandez@gmail.com>
2023-06-21 12:19:01 -0500

    Update test.php

--- test.php
+++ test.php
@@ -1,4 +1,5 @@
 <?php
+print("v1")
 // PHP Data Objects(PDO) Sample Code:
 try {
     $conn = new PDO("sqlsrv:server = tcp:mirarlol.database.windows.net,1433; Database = champion_db", "joacoenriqueconrado1", "Siempregptarqui1");
