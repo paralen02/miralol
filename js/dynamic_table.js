c0b44d9e3400b8dd46fa372af549844e9684dc03
paralen02 <u202115142@upc.edu.pe>
2023-06-21 16:34:49 -0500

    images

--- js/dynamic_table.js
+++ js/dynamic_table.js
@@ -61,7 +61,7 @@ function renderTable(championData) {
       var row = $('<tr>');
 
     // Append image for the champion
-    var imageName = champion.Name.replace(/[' .]/g, "");
+    var imageName = encodeURIComponent(champion.Name);
     var imageSrc = "championsgallery/" + imageName + ".png";
     var imageCell = $('<td>');
     var image = new Image(); // Create new HTMLImageElement instance
