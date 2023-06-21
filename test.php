ddcf2a3f5681df39e6e8c9cf0c66f7205ede2667
Conrado F. Moreno-Fernández <cfmorenofernandez@gmail.com>
2023-06-21 13:13:25 -0500

    Update test.php

--- test.php
+++ test.php
@@ -4,14 +4,36 @@ try {
     $conn = new PDO("sqlsrv:server = tcp:mirarlol.database.windows.net,1433; Database = champion_db", "joacoenriqueconrado1", "Siempregptarqui1");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // Execute the GetStatistics procedure
-    $stmt = $conn->query("EXEC GetStatistics");
+    // Execute the DataStatistics procedure
+    $stmt = $conn->query("EXEC DataStatistics");
 
     // Fetch all rows from the result set
+    $rows = [];
     while ($result = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        // Log each row in the console
-        echo json_encode($result);;
+        $rows[] = $result;
     }
+
+    // Define the file name for the CSV
+    $filename = "results.csv";
+
+    // Set the Content-Type and Content-Disposition headers
+    header("Content-Type: text/csv");
+    header("Content-Disposition: attachment; filename=\"$filename\"");
+
+    // Open a file pointer using the php://output IO stream
+    $fp = fopen("php://output", "w");
+
+    // Add the column names as headers to the CSV file
+    $columnNames = array_keys($rows[0]);
+    fputcsv($fp, $columnNames);
+
+    // Loop through the rows and add them to the CSV file
+    foreach ($rows as $row) {
+        fputcsv($fp, $row);
+    }
+
+    // Close the file pointer
+    fclose($fp);
 } catch (PDOException $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
